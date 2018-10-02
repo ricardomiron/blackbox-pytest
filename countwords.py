a9d689494b04c50902f016ee8eda67cfa4d3a53e
Ricardo Mirón <r.mirontorres@ugto.mx>
2018-10-03 01:42:42 +0200

    Added user input

--- countwords.py
+++ countwords.py
@@ -1,11 +1,13 @@
 from collections import Counter
 def main():
 
+    file = input('Enter the name of the text file: ')
+
     with open(r'exceptions.txt') as e:
 
         exceptions = [exception for line in e for exception in line.split()]
 
-    with open(r'test.txt') as f:
+    with open(file) as f:
 
         words = [word for line in f for word in line.split()]
         words = [x for x in words if x not in exceptions]
