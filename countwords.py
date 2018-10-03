3ad0546abf974f003adc38cce69a1cda581e34d6
Ricardo Mirón <r.mirontorres@ugto.mx>
2018-10-03 03:16:36 +0200

    Support for multiple text files at the time

--- countwords.py
+++ countwords.py
@@ -1,19 +1,26 @@
 from collections import Counter
 def main():
 
-    file = input('Enter the name of the text file: ')
+    #user inputs file names separated by spaces
+    file = input('Enter the name of the text file(s): ')
+    files = file.split(" ")
 
+    #read the file with the words we want to exclude and make a list
     with open(r'exceptions.txt') as e:
-
         exceptions = [exception for line in e for exception in line.split()]
 
-    with open(file) as f:
+    #iterate over each file name
+    for y in files:
 
-        words = [word for line in f for word in line.split()]
-        words = [x for x in words if x not in exceptions]
-        print("The total word count is:", len(words))
+        with open(y) as f:
 
-        c = Counter(words)
-        for word, count in c.most_common():
-            print(word, count)
+            words = [word for line in f for word in line.split()]
+            #ignore the intersection between the current list and the excpetions list
+            words = [x for x in words if x not in exceptions]
+            print("\nThe total word count is:", len(words), "from file", y)
+
+            c = Counter(words)
+            #show list of words by the frequency of appearance
+            for word, count in c.most_common():
+                print(word, count)
 main()
